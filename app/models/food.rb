07577dc68c2980dd42d2923d99b34139456e2107
Ambara Yasa <81506775+ambarayasa@users.noreply.github.com>
2022-04-12 16:59:22 +0000

    Add validation for Food, not accept non numeric values for price & not accept price less than 0.01

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,11 @@
 class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0.01}
+
+  belongs_to :category
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
